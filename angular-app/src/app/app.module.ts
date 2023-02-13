77118ef55cafcd45af201d68fe6efd263f63a5ef
Michael Gichuhi <gmikekamau@gmail.com>
2023-02-13 15:56:35 +0300

    moved the button component to comoponents folder and made mock data

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TestComponent } from './components/test/test.component';
 import { HomeComponent } from './components/home/home.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
