83308bed799f7348d350e1c7131f2c9d2fa1a672
Michael Gichuhi <gmikekamau@gmail.com>
2023-02-16 19:30:51 +0300

    working on the services component

--- angular-app/src/app/components/tasks/tasks.component.ts
+++ angular-app/src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { Task } from 'src/app/Task';
-import { TaskService } from 'src/app/services/task.service';
+import { TaskService } from 'src/app/components/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
-  tasks: Task[] = []
+  tasks: Task[] = [];
 
-  constructor( private taskService:TaskService){
-
-  }
-
+  constructor(private taskService: TaskService) {}
 }
